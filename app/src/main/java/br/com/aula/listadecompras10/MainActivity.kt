1f3adfcf7e54928da02652351be0241ee9093e72
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-07-07 22:01:49 -0300

    Initial commit

---
+++ app/src/main/java/br/com/aula/listadecompras10/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.aula.listadecompras10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
