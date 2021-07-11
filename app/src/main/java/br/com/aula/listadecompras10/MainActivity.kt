c214cc8645aaece1b81a2f4d1e67c27dfd35dc6b
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-07-11 11:37:58 -0300

    Implementada a chamada para a tela de cadastro.

--- app/src/main/java/br/com/aula/listadecompras10/MainActivity.kt
+++ app/src/main/java/br/com/aula/listadecompras10/MainActivity.kt
@@ -1,11 +1,28 @@
 package br.com.aula.listadecompras10
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_cadastro.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_adicionarr : Button
+        btn_adicionarr = btn_adicionar
+        btn_adicionarr.setOnClickListener(object : View.OnClickListener
+        {
+            override fun onClick(p0: View?)
+            {
+                val intent = Intent(this@MainActivity, CadastroActivity ::class.java)
+                startActivity(intent)
+            }
+        })
     }
 }
